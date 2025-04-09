5fddd53c685ff381ec7235edffdcb21f8658d087
sun.wang <sun@ASUS-DESKTOP.localdomain>
2025-04-09 15:26:33 +0800

    [fix] fix sc git pull about

--- build_all_openwrt.sh
+++ build_all_openwrt.sh
@@ -13,39 +13,56 @@ BUILD_DIR="${WORK_DIR}/openwrt"
 
 if [[ ! -d ${WORK_DIR}/openwrt_build_config ]];then
     git clone https://github.com/sunowsir/openwrt_build_config.git || exit
+else 
+    cd "${WORK_DIR}/openwrt_build_config" || exit
+    git pull || exit
+    cd "${WORK_DIR}" || exit
 fi
 
 if [[ ! -d ${WORK_DIR}/feeds.conf.default ]]; then
     git clone https://github.com/sunowsir/feeds.conf.default.git || exit 
+else 
+    cd "${WORK_DIR}/feeds.conf.default" || exit
+    git pull || exit
+    cd "${WORK_DIR}" || exit
 fi
 
 if [[ ! -d ${WORK_DIR}/openwrt_patch ]]; then
     git clone https://github.com/sunowsir/openwrt_patch.git || exit 
+else 
+    cd "${WORK_DIR}/openwrt_patch" || exit
+    git pull || exit
+    cd "${WORK_DIR}" || exit
 fi
 
 if [[ ! -d ${BUILD_DIR} ]]; then
     git clone -b openwrt-24.10 https://git.openwrt.org/openwrt/openwrt.git || exit 
+else 
+    cd "${BUILD_DIR}" || exit
+    git pull  || exit
+    cd "${WORK_DIR}" || exit
 fi
 
 cp "${WORK_DIR}/feeds.conf.default/feeds.conf.default" "${BUILD_DIR}" || exit
 
-cp -r "${WORK_DIR}/openwrt_qcow2_build_support/*" "${BUILD_DIR}" || exit
+cp -r "${WORK_DIR}/openwrt_patch/*" "${BUILD_DIR}" || exit
 
 cd "${BUILD_DIR}" || exit
 
-git pull && ./scripts/feeds update -a && ./scripts/feeds install -a
-git pull && ./scripts/feeds update -a && ./scripts/feeds install -a
-git pull && ./scripts/feeds update -a && ./scripts/feeds install -a
+./scripts/feeds update -a && ./scripts/feeds install -a
+./scripts/feeds update -a && ./scripts/feeds install -a
+./scripts/feeds update -a && ./scripts/feeds install -a
 
 cp "${WORK_DIR}/openwrt_build_config/config.buildinfo" "${BUILD_DIR}/.config" || exit
 
 make defconfig || exit 
 
-make -j12 download || make -j12 download PKG_HASH=skip || exit
 
-make -j12 prepare  || exit 
+make -j"$(nproc)" download || exit
 
-make -j12 || make -j12 PKG_HASH=skip || exit 
+make -j"$(nproc)" prepare  || exit 
+
+make -j"$(nproc)" || exit 
 
 set +x 
 
